61dd26958bbec5845a097804307ec2f94cf52687
zlata16 <zlataimaeva@gmail.com>
2021-04-20 11:49:48 +0700

    asd

---
+++ asset/js/main.js
@@ -0,0 +1,30 @@
+//alert("hello, word!")
+
+/*
+comment
+*/
+
+//создание переменных
+//1. var
+//2. let
+
+
+
+var colorArray = ["#5A9C6E", "#A8BF5A", "#CCC", "#000", "#00FF00"];
+var i = 0;
+function changeColor() {
+	document.body.style.background = colorArray[i];
+	i++;
+
+    console.log(colorArray[i]);   
+
+	if (i > colorArray.length -1) {
+		i = 0;
+	}
+}
+
+function selectColor() {
+	let color = document.getElementById("color").value;
+	document.body.style.background = color;
+	console.log(color);
+}

421ba121696ed09fa60f8f3a418a71291ed340bd
zlata16 <zlataimaeva@gmail.com>
2021-04-21 15:45:45 +0700

    asd

--- asset/js/main.js
+++ asset/js/main.js
@@ -23,8 +23,13 @@ function changeColor() {
 	}
 }
 
+
 function selectColor() {
 	let color = document.getElementById("color").value;
 	document.body.style.background = color;
-	console.log(color);
+	document.getElementById('result').innerHTML = color;
+
+	//console.log(color);
 }
+let oper_elements = document.getElementsByClassName('oper');
+	console.log(oper_elements); 

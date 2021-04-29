c3570611167f57dde608439d003888469fc0dd8e
zlata16 <zlataimaeva@gmail.com>
2021-04-29 13:56:51 +0700

    asd

--- asset/js/main.js
+++ asset/js/main.js
@@ -29,7 +29,32 @@ function selectColor() {
 	document.body.style.background = color;
 	document.getElementById('result').innerHTML = color;
 
-	//console.log(color);
 }
 let oper_elements = document.getElementsByClassName('oper');
 	console.log(oper_elements); 
+let numbers = document.getElementsByClassName('oper');
+	console.log(oper_elements); 
+
+for (var i = 0; i < oper_elements.length; i++) {
+	oper_elements[i].addEventListener('click', oper);
+}
+for (var i = 0; i < numbers.length; i++) {
+	numbers[i].addEventListener('click', addNumber);
+}
+
+
+function oper() {
+	let o= this.value;
+	//console.log(o);
+	add=0;
+
+	if (o== '=') {
+		//alert(o);
+		//calc();
+	}
+}
+function addNumber(v) {
+let input = document.getElementById('enter');
+let o = input.value;
+input.value = o + v;
+}

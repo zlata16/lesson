0218a3addfe7444051f922c7bddb931883adc463
zlata16 <zlataimaeva@gmail.com>
2021-05-10 21:58:10 +0700

    .

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,60 +1,26 @@
-//alert("hello, word!")
+//alert("Hello world!");
 
-/*
-comment
-*/
-
-//создание переменных
-//1. var
-//2. let
-
-
-
-var colorArray = ["#5A9C6E", "#A8BF5A", "#CCC", "#000", "#00FF00"];
-var i = 0;
+var colorArray = ["#5A9C6E", 
+					"#A8BF5A",
+ 					"#CCC", 
+ 					"#000",
+ 					"00ff00"];
+ var i = 0;
 function changeColor() {
 	document.body.style.background = colorArray[i];
 	i++;
-
-    console.log(colorArray[i]);   
-
-	if (i > colorArray.length -1) {
+	//console.log(colorArray[i]);
+	if(i > colorArray.length - 1){
 		i = 0;
 	}
 }
 
-
 function selectColor() {
+	// body...
 	let color = document.getElementById("color").value;
 	document.body.style.background = color;
+	// console.log(color);
 	document.getElementById('result').innerHTML = color;
-
 }
-let oper_elements = document.getElementsByClassName('oper');
-	console.log(oper_elements); 
-let numbers = document.getElementsByClassName('oper');
-	console.log(oper_elements); 
-
-for (var i = 0; i < oper_elements.length; i++) {
-	oper_elements[i].addEventListener('click', oper);
-}
-for (var i = 0; i < numbers.length; i++) {
-	numbers[i].addEventListener('click', addNumber);
-}
-
-
-function oper() {
-	let o= this.value;
-	//console.log(o);
-	add=0;
-
-	if (o== '=') {
-		//alert(o);
-		//calc();
-	}
-}
-function addNumber(v) {
-let input = document.getElementById('enter');
-let o = input.value;
-input.value = o + v;
-}
+	let oper_elements = document.getElementsByClassName('oper');
+	console.log(oper_elements);
